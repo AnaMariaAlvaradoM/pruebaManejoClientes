34c5da70206427cb506d650434d94564f74c0e24
AnaMAriaAlvaradoM <anaa_alvarado@hotmail.com>
2024-11-06 08:54:59 -0500

    primer

---
+++ ManejoClientes/src/main/java/com/example/manejo_clientes/services/ClienteService.java
@@ -0,0 +1,36 @@
+package com.example.manejo_clientes.services;
+
+
+import com.example.manejo_clientes.model.Cliente;
+import com.example.manejo_clientes.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente createCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente getClienteById(Long id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente updateCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public void deleteCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
